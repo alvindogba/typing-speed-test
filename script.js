86443c1d9ca6face4e045746a355c584865cf0cb
unknown <alvindr2000@gmail.com>
2024-06-27 00:48:59 +0000

    second Update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // Getting elements from the DOM
-let para_tobe_type = document.getElementById("paragraph");
+let para_tobe_type = document.querySelector(".display_text");
+let viewBoard = document.querySelector(".view_board");
 let duration = document.getElementById("time_left");
 let error = document.getElementById("error");
 let wmp = document.getElementById("wpm");
@@ -8,8 +9,31 @@ let accuracy = document.getElementById("accuracy");
 let cmp = document.getElementById("cpm");
 let startButton = document.getElementById("start_Game");
 
-let para;
+// setting Global variable
+
+
+let time = 60;
+
+function CountDown(){
+    duration.textContent =time + "s";
+    if (time > 0){
+    time--;
+    setTimeout(CountDown, 1000);
+}else{
+    duration.textContent = 'Time\'s up!';
+    alert("game over")
+    duration.textContent = "";
+    viewBoard.textContent= "";
+    para_tobe_type.textContent="";
+    clearTimeout(CountDown)
+}
+
+
+}
+
+
 let keys = [];
+let allError=[];
 let globalParagraph = '';
 
 // Event listener for the start button to fetch and display a random paragraph
@@ -23,7 +47,11 @@ startButton.addEventListener("click", () => {
         para_tobe_type.textContent = paragraphs[randomNumber];
         globalParagraph = paragraphs[randomNumber];
         keys = []; // Reset keys array for a new paragraph
+        viewBoard.innerHTML="";
+        
+      ;
       });
+      CountDown()
 });
 
 // Keyboard event listener to capture user input
@@ -35,7 +63,8 @@ document.addEventListener("keyup", (e) => {
     } else {
         console.log("Error: Invalid key pressed.");
     }
-})
+   
+});
 
 // Function to validate user key press against the global paragraph
 function validateKeyPress() {
@@ -46,11 +75,24 @@ function validateKeyPress() {
         let el = document.createElement("span");
         el.classList.add("correct");
         el.textContent = keys[lengthOfKeyPress - 1];
-        para_tobe_type.appendChild(el);
-    } else {
+        viewBoard.appendChild(el);
+    }else if(!matchKeys){
         let el = document.createElement("span");
         el.classList.add("wrong");
         el.textContent = keys[lengthOfKeyPress - 1];
-        para_tobe_type.appendChild(el);
+        viewBoard.appendChild(el);
+          //Pushing all the error to the error  array;
+        allError.push(el.textContent)
+        // Updating the UI with the user Error
+        error.textContent=allError.length;
+        
+    }
+    else {
+        let el = document.createElement("span");
+        el.classList.add("wrong");
+        el.textContent = keys[lengthOfKeyPress - 1];
+        viewBoard.appendChild(el);
+        
+      
     }
 }
